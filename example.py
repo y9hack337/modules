3dfe84967c65b8d0641e963cc8877845c87a9f7d
Hack337 <69693943+y9hack337@users.noreply.github.com>
2025-03-15 15:40:35 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 from pyrogram import Client, filters
+from pyrogram.types import Message
 import configparser
 
 commands = [
@@ -20,15 +21,15 @@ prefix_userbot = config['HACK337_USERBOT']['prefix_userbot'].split(',')
 
 def register_commands(app):
     @app.on_message(filters.me & filters.command("hi", prefixes=prefix_userbot))
-    async def hi_module(client, message):
+    async def hi_module(client: Client, message: Message):
         await message.edit("Привет!")
     
     @app.on_message(filters.me & filters.command("bye", prefixes=prefix_userbot))
-    async def bye_module(client, message):
+    async def bye_module(client: Client, message: Message):
         await message.edit("Пока!")
     
     @app.on_message(filters.me & filters.command("test", prefixes=prefix_userbot))
-    async def test_module(client, message):
+    async def test_module(client: Client, message: Message):
         await message.edit("Пока!")
 
 print("Модуль hi/bye загружен!")
